d4f487b1cabd6a849d032923b007b953b5e82e50
ikebets <ilia.kebets@epfl.ch>
2014-10-22 11:07:51 +0200

    implement Persistance Trait

--- src/main/scala/ch/epfl/ts/first/Persistance.scala
+++ src/main/scala/ch/epfl/ts/first/Persistance.scala
@@ -1,5 +1,22 @@
 package ch.epfl.ts.first
 
+import ch.epfl.ts.data.Transaction
+import ch.epfl.ts.data.Order
+
+
 trait Persistance[T] {
+  
+  
+  def save(t: Transaction)
+  def save(ts: List[Transaction])
+  
+  def loadTransaction(id: Int) : Transaction
+  def loadTransactions(ids: List[Int]) : List[Transaction]
+  
+  def save(o: Order)
+  def save(os: List[Order])
+  
+  def loadOrder(id: Int) : Order
+  def loadOrders(ids: List[Int]) : List[Order]
 
 }
