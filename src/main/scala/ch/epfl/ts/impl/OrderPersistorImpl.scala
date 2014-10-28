ec091c7a837b37bf0c07d4a148cf96ea3b852f71
Marzell Camenzind <marzell.camenzind@gmail.com>
2014-10-28 14:18:23 +0100

    Saving

--- src/main/scala/ch/epfl/ts/impl/OrderPersistorImpl.scala
+++ src/main/scala/ch/epfl/ts/impl/OrderPersistorImpl.scala
@@ -1,20 +1,20 @@
 package ch.epfl.ts.impl
 
-import ch.epfl.ts.first.Persistor
+import ch.epfl.ts.first.Persistance
 import ch.epfl.ts.data.Order
 import java.util.ArrayList
-import scala.slick.driver.SQLiteDriver
-import scala.slick.jdbc.StaticQuery
-import scala.slick.jdbc.meta.MTable
-import scala.slick.jdbc.JdbcBackend.Database
-import scala.slick.lifted.TableQuery
-import scala.slick.direct.AnnotationMapper.column
-import scala.slick.lifted.Tag
-import scala.slick.model.Table
-import ch.epfl.ts.first.Persistor
-//import scala.slick.
 
-class OrderPersistorImpl extends Persistor[Order] {
+//import scala.slick.driver.SQLiteDriver
+//import scala.slick.jdbc.StaticQuery
+//import scala.slick.jdbc.meta.MTable
+//import scala.slick.direct.AnnotationMapper.column
+
+import scala.slick.jdbc.JdbcBackend.Database
+
+import scala.slick.lifted.{Tag, TableQuery, Column}
+import scala.slick.driver.SQLiteDriver._
+
+class OrderPersistorImpl extends Persistance[Order] {
 
   def init() = {
     val db = Database.forURL("jdbc:sqlite:testDB.txt", driver = "org.sqlite.JDBC")
@@ -22,12 +22,12 @@ class OrderPersistorImpl extends Persistor[Order] {
 
   // Definition of the SUPPLIERS table
   class Suppliers(tag: Tag) extends Table[(Int, String, String, String, String, String)](tag, "SUPPLIERS") {
-    def id = column[Int]("SUP_ID", O.PrimaryKey) // This is the primary key column
-    def name = column[String]("SUP_NAME")
-    def street = column[String]("STREET")
-    def city = column[String]("CITY")
-    def state = column[String]("STATE")
-    def zip = column[String]("ZIP")
+    def id: Column[Int] = column[Int]("SUP_ID", O.PrimaryKey) // This is the primary key column
+    def name: Column[String] = column[String]("SUP_NAME")
+    def street: Column[String] = column[String]("STREET")
+    def city: Column[String] = column[String]("CITY")
+    def state: Column[String] = column[String]("STATE")
+    def zip: Column[String] = column[String]("ZIP")
     // Every table needs a * projection with the same type as the table's type parameter
     def * = (id, name, street, city, state, zip)
   }
