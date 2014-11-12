82a0cd1dd7256b2a3312ce3760c293103ba4c906
Marzell Camenzind <marzell.camenzind@gmail.com>
2014-11-12 10:58:39 +0100

    cto clean up

--- src/main/scala/ch/epfl/ts/data/Transaction.scala
+++ src/main/scala/ch/epfl/ts/data/Transaction.scala
@@ -3,5 +3,5 @@ package ch.epfl.ts.data
 import Currency._
 
 case class Transaction(price: Double, quantity: Double, timestamp: Long, currency: Currency, buyer: String, seller: String) {
-  override def toString: String = {"Transaction: price=" + price + ", quantity=" + quantity + ", timestamp=" + timestamp + ", currency=" + currency + ", buyer=" + buyer + ", seller=" + seller}
+  override def toString: String = "Transaction: price=" + price + ", quantity=" + quantity + ", timestamp=" + timestamp + ", currency=" + currency + ", buyer=" + buyer + ", seller=" + seller
 }
