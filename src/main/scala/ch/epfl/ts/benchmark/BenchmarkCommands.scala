d156b9b55cb277e13d7aa07ae03afe14e8fb17af
Marzell Camenzind <marzell.camenzind@gmail.com>
2014-11-25 15:28:51 +0100

    Added benchmark start Start object and Report object

---
+++ src/main/scala/ch/epfl/ts/benchmark/BenchmarkCommands.scala
@@ -0,0 +1,4 @@
+package ch.epfl.ts.benchmark
+
+case class Run(offset: Long)
+case class Report(startTime: Long, endTime: Long)
--- src/main/scala/ch/epfl/ts/benchmark/FetchActors.scala
+++ src/main/scala/ch/epfl/ts/benchmark/FetchActors.scala
@@ -1,24 +1,32 @@
 package ch.epfl.ts.benchmark
 
-import akka.actor.{ActorRef, Actor}
+import akka.actor.{Actor, ActorRef}
 import ch.epfl.ts.data.{Currency, Transaction}
 
+import scala.concurrent.duration.FiniteDuration
 import scala.io.Source
 
-class FileFetchActor(destination: ActorRef, filename: String) extends Actor {
+abstract class TimedReporterActor(master: ActorRef, dest: ActorRef) extends Actor {
   def receive = {
-    case Run => readAndSend
+    case c: Run => this.context.system.scheduler.scheduleOnce(FiniteDuration(c.offset, scala.concurrent.duration.MILLISECONDS)) {
+      readAndSend
+    }
   }
-  def readAndSend = {
+  def readAndSend: Unit = {
     val startTime = System.currentTimeMillis
-    val source = Source.fromFile(filename)
-    val lines = source.getLines().toList
-    lines.map(_.split(",")).map(
-      l => destination ! Transaction(l(1).toDouble, l(2).toDouble, l(0).toLong, Currency.withName(l(3).toLowerCase), l(4), l(5))
-    )
+    f()
     val endTime = System.currentTimeMillis
-
+    master ! Report(startTime, endTime)
   }
+  def f(): Unit
 }
 
-case class Run()
+class FileFetchActor(master: ActorRef, destination: ActorRef, filename: String)
+  extends TimedReporterActor(master, destination) {
+  override def f = Source.fromFile(filename).getLines().foreach(
+    s => {
+      val l = s.split(",")
+      destination ! Transaction(l(1).toDouble, l(2).toDouble, l(0).toLong, Currency.withName(l(3).toLowerCase), l(4), l(5))
+    }
+  )
+}
