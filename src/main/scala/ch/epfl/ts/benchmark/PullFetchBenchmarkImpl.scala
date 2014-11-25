524503eeefd8c0f8462f201997008aeb947af878
Marzell Camenzind <marzell.camenzind@gmail.com>
2014-11-25 14:28:21 +0100

    Pull fetcher file reader for benchmark

--- src/main/scala/ch/epfl/ts/benchmark/PullFetchBenchmarkImpl.scala
+++ src/main/scala/ch/epfl/ts/benchmark/PullFetchBenchmarkImpl.scala
@@ -1,14 +1,26 @@
 package ch.epfl.ts.benchmark
 
+import ch.epfl.ts.data.{Currency, Transaction}
+
 import scala.io.Source
 import ch.epfl.ts.first.PullFetch
 
-class PullFetchBenchmarkImpl extends PullFetch[String] {
-override def interval = 12000
+class PullFetchBenchmarkImpl extends PullFetch[Transaction] {
+override def interval = 12000 * 1000 * 1000
 
-  val source = Source.fromURL("/BITCOIN-BITSTAMPUSD.csv")
+  val filename = "/BITCOIN-BITSTAMPUSD.csv"
+  var called = false
 
-  override def fetch: List[String] = {
-    return source.getLines().toList
+  override def fetch: List[Transaction] = {
+    if (called) {
+      List[Transaction]()
+    } else {
+      called = true
+      val source = Source.fromFile(filename)
+      val lines = source.getLines().toList
+      lines.map(_.split(",")).map(
+        l => Transaction(l(1).toDouble, l(2).toDouble, l(0).toLong, Currency.withName(l(3).toLowerCase), l(4), l(5))
+      )
+    }
   }
 }
