2fb3c344a386f73ae86489812a03d84941342d2f
Marzell Camenzind <marzell.camenzind@gmail.com>
2014-12-10 19:11:18 +0100

    Simplified Fetcher and its actors

--- src/main/scala/ch/epfl/ts/first/Fetch.scala
+++ src/main/scala/ch/epfl/ts/first/Fetch.scala
@@ -11,14 +11,8 @@ abstract class PullFetch[T] extends Fetch[T] {
   def fetch(): List[T]
   def interval(): Int
 }
-//abstract class TransactionPullFetch extends PullFetch[Transaction]
-//abstract class OrderPullFetch extends PullFetch[Order]
-
 /* Direction PUSH */
 abstract class PushFetch[T] (callback: T => Unit) extends Fetch[T]
-//abstract class TransactionPushFetch(callback: Transaction => Unit) extends PushFetch[Transaction](callback)
-//abstract class OrderPushFetch(callback: Order => Unit) extends PushFetch[Order](callback)
-
 
 /* Actor implementation */
 protected[first] class PullFetchActor[T](f: PullFetch[T], dest: List[ActorRef]) extends Actor {
@@ -30,11 +24,6 @@ protected[first] class PullFetchActor[T](f: PullFetch[T], dest: List[ActorRef]) 
     case Fetch => f.fetch().map( t => dest.map(_ ! t))
   }
 }
-/*
-protected[first] class TransactionPullFetchActor(f: PullFetch[Transaction], dest: List[ActorRef])
-  extends PullFetchActor[Transaction](f, dest)
-protected[first] class OrderPullFetchActor(f: PullFetch[Order], dest: List[ActorRef])
-  extends PullFetchActor[Order](f, dest)*/
 
 /* Actor implementation */
 protected[first] class PushFetchActor[T](f: PushFetch[T], dest: ActorRef) extends Actor {
