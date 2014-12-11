b50e477e50cb3af9c05a40dbe52b46edbc066d21
Marzell Camenzind <marzell.camenzind@gmail.com>
2014-12-11 09:46:00 +0100

    Clean up Live flow tester with DB Storage

--- src/main/scala/ch/epfl/main/LiveFlowTesterWithStorage.scala
+++ src/main/scala/ch/epfl/main/LiveFlowTesterWithStorage.scala
@@ -1,16 +1,12 @@
-
-
 package ch.epfl.main
 
+import akka.actor._
 import ch.epfl.ts.data.Transaction
 import ch.epfl.ts.first.InStage
-import akka.actor._
 import ch.epfl.ts.first.fetcher.BtceTransactionPullFetcher
 import ch.epfl.ts.impl.TransactionPersistorImpl
 
-object FlowTester {
-  
-  
+object LiveFlowTesterWithStorage {
   // Stage 2, used just to print out the result from stage 1
   class Printer extends Actor {
     override def receive = {
@@ -18,26 +14,14 @@ object FlowTester {
       case _  => 
     }
   }
-  
-  
-  
+
   def main(args: Array[String]) = {
     val system = ActorSystem("DataSourceSystem")
-  
     val printer = system.actorOf(Props(classOf[Printer]), "instage-printer")
-
     val persistor = new TransactionPersistorImpl()
     persistor.init()
-
     val instage = (new InStage[Transaction](system, List(printer)))
       .withPersistance(persistor)
       .withFetchInterface(new BtceTransactionPullFetcher()).start
-
-    /*
-    val is = system.actorOf(Props(classOf[InStage[Transaction]], List(printer), 
-        , new TransactionPersistorImpl() ), "instage-inst")
-    is ! "init"
-    */
   }
-
 }
