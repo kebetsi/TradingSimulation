d8f0487dbdb6b440bc37f4486ab95ba62b23ea6d
ikebets <ilia.kebets@epfl.ch>
2014-12-26 07:40:02 +0100

    minor print issue fix.

--- src/main/scala/ch/epfl/ts/data/Order.scala
+++ src/main/scala/ch/epfl/ts/data/Order.scala
@@ -12,7 +12,7 @@ import OrderType._
 
 case class Order (id: Long, price: Double, quantity: Double, timestamp: Long, currency: Currency, orderType: OrderType)
   extends StreamObject {
-  override def toString: String = "Order: timestamp=" + timestamp + ", id=" + id + ", orderType=" + orderType + ", volume=" + timestamp + ", volume=" + quantity + ", price=" + orderType + ", currency=" + currency
+  override def toString: String = "Order: timestamp=" + timestamp + ", id=" + id + ", orderType=" + orderType + ", volume=" + timestamp + ", volume=" + quantity + ", price=" + price + ", currency=" + currency
 }
 
 
