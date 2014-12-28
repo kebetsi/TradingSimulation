9a2404db05fdd31f2990946910edb02b1cdf77a5
Marzell Camenzind <marzell.camenzind@gmail.com>
2014-12-28 15:19:37 +0100

    Minor cleanup in example.

--- src/main/scala/ch/epfl/ts/example/LiveFlowTesterWithStorage.scala
+++ src/main/scala/ch/epfl/ts/example/LiveFlowTesterWithStorage.scala
@@ -2,10 +2,9 @@ package ch.epfl.ts.example
 
 import akka.actor.Props
 import ch.epfl.ts.component.fetch.BtceTransactionPullFetcherComponent
+import ch.epfl.ts.component.persist.{TransactionPersistorImpl, TransactionPersistanceComponent, Persistance}
 import ch.epfl.ts.component.{Component, ComponentBuilder}
 import ch.epfl.ts.data.Transaction
-import ch.epfl.ts.first.{Persistance, TransactionPersistanceComponent}
-import ch.epfl.ts.impl.TransactionPersistorImpl
 
 /**
  * Demonstration of fetching Live Bitcoin/USD trading data from BTC-e,
@@ -15,10 +14,10 @@ object LiveFlowTesterWithStorage {
   def main(args: Array[String]): Unit = {
     implicit val builder = new ComponentBuilder("DataSourceSystem")
 
-    val transacPerst: Persistance[Transaction] = new TransactionPersistorImpl("FlowTester")
+    val transacPersist: Persistance[Transaction] = new TransactionPersistorImpl("FlowTester")
 
     val printer = builder.createRef(Props(classOf[Printer], "my-printer"))
-    val persistor = builder.createRef(Props(classOf[TransactionPersistanceComponent], transacPerst))
+    val persistor = builder.createRef(Props(classOf[TransactionPersistanceComponent], transacPersist))
     val fetcher = builder.createRef(Props(classOf[BtceTransactionPullFetcherComponent], "my-fetcher"))
 
     fetcher.addDestination(printer, classOf[Transaction])
@@ -33,7 +32,7 @@ object LiveFlowTesterWithStorage {
  * @param name The name of the component.
  */
 class Printer(val name: String) extends Component {
-  def receiver = {
+  override def receiver = {
     case t: Transaction => println(System.currentTimeMillis, t.toString)
     case _ =>
   }
