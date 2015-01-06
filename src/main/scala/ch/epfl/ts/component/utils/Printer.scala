666757fe060d5d0ee7f9950c6ec396eb7ec6373a
ikebets <ilia.kebets@epfl.ch>
2015-01-06 11:42:15 +0100

    printer message refactor.

--- src/main/scala/ch/epfl/ts/component/utils/Printer.scala
+++ src/main/scala/ch/epfl/ts/component/utils/Printer.scala
@@ -11,9 +11,9 @@ class Printer(val name: String) extends Component {
   override def receiver = {
     case t: Transaction    => println("Printer: Transaction\t" + System.currentTimeMillis + "\t" + t.toString)
     case t: Tweet          => println("Printer: Tweet\t" + System.currentTimeMillis + "\t" + t.toString)
-    case lb: LimitBidOrder => println("Printer: LB\t" + System.currentTimeMillis() + "\t" + lb.toString)
-    case la: LimitAskOrder => println("Printer: LA\t" + System.currentTimeMillis() + "\t" + la.toString)
-    case del: DelOrder     => println("Printer: Del\t" + System.currentTimeMillis() + "\t" + del.toString)
+    case lb: LimitBidOrder => println("Printer: Limit Bid Order\t" + System.currentTimeMillis() + "\t" + lb.toString)
+    case la: LimitAskOrder => println("Printer: Limit Ask Order\t" + System.currentTimeMillis() + "\t" + la.toString)
+    case del: DelOrder     => println("Printer: Delete Order\t" + System.currentTimeMillis() + "\t" + del.toString)
     case _                 => println("Printer: received unknown")
   }
 }
