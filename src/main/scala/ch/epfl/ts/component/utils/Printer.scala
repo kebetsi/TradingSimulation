501345c64183c7982186af74e9c543946206f823
ikebets <ilia.kebets@epfl.ch>
2015-01-11 15:28:38 +0100

    printer display its name

--- src/main/scala/ch/epfl/ts/component/utils/Printer.scala
+++ src/main/scala/ch/epfl/ts/component/utils/Printer.scala
@@ -9,11 +9,11 @@ import ch.epfl.ts.data.{DelOrder, LimitAskOrder, LimitBidOrder, Transaction, Twe
  */
 class Printer(val name: String) extends Component {
   override def receiver = {
-    case t: Transaction    => println("Printer: Transaction\t" + System.currentTimeMillis + "\t" + t.toString)
-    case t: Tweet          => println("Printer: Tweet\t" + System.currentTimeMillis + "\t" + t.toString)
-    case lb: LimitBidOrder => println("Printer: Limit Bid Order\t" + System.currentTimeMillis() + "\t" + lb.toString)
-    case la: LimitAskOrder => println("Printer: Limit Ask Order\t" + System.currentTimeMillis() + "\t" + la.toString)
-    case del: DelOrder     => println("Printer: Delete Order\t" + System.currentTimeMillis() + "\t" + del.toString)
-    case _                 => println("Printer: received unknown")
+    case t: Transaction    => println("Printer " + name + ": Transaction\t" + System.currentTimeMillis + "\t" + t.toString)
+    case t: Tweet          => println("Printer " + name + ": Tweet\t" + System.currentTimeMillis + "\t" + t.toString)
+    case lb: LimitBidOrder => println("Printer " + name + ": Limit Bid Order\t" + System.currentTimeMillis() + "\t" + lb.toString)
+    case la: LimitAskOrder => println("Printer " + name + ": Limit Ask Order\t" + System.currentTimeMillis() + "\t" + la.toString)
+    case del: DelOrder     => println("Printer " + name + ": Delete Order\t" + System.currentTimeMillis() + "\t" + del.toString)
+    case _                 => println("Printer " + name + ": received unknown")
   }
 }
