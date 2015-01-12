c00a55ec0c86e4ba5b5ee27835bcb352410ca32b
ikebets <ilia.kebets@epfl.ch>
2015-01-12 10:08:59 +0100

    add marketId param to btc fetchers.

--- src/main/scala/ch/epfl/ts/example/BtceTransactionFlowTesterWithStorage.scala
+++ src/main/scala/ch/epfl/ts/example/BtceTransactionFlowTesterWithStorage.scala
@@ -2,8 +2,8 @@ package ch.epfl.ts.example
 
 import akka.actor.Props
 import ch.epfl.ts.component.ComponentBuilder
-import ch.epfl.ts.component.fetch.{BtceTransactionPullFetcher, PullFetchComponent}
-import ch.epfl.ts.component.persist.{Persistor, TransactionPersistor}
+import ch.epfl.ts.component.fetch.{ BtceTransactionPullFetcher, PullFetchComponent, BitstampTransactionPullFetcher }
+import ch.epfl.ts.component.persist.{ Persistor, TransactionPersistor }
 import ch.epfl.ts.component.utils.Printer
 import ch.epfl.ts.data.Transaction
 
@@ -18,20 +18,29 @@ object BtceTransactionFlowTesterWithStorage {
     implicit val builder = new ComponentBuilder("DataSourceSystem")
 
     // Initialize the Interface to DB
-    val btceXactPersit = new TransactionPersistor("btce-transaction-db")
+    val btceXactPersit = new TransactionPersistor("btce-transaction-db-batch")
     btceXactPersit.init()
+    val bitstampXactPersit = new TransactionPersistor("bitstamp-transaction-db-batch")
+    bitstampXactPersit.init()
 
-    // Instantiate a Transaction etcher for BTC-e
-    val btcePullFetcher = new BtceTransactionPullFetcher()
+    // Instantiate a Transaction etcher for BTC-e and Bitstamp
+    val btceMarketId= 1
+    val bitstampMarketId= 2
+    val btcePullFetcher = new BtceTransactionPullFetcher(btceMarketId)
+    val bitstampPullFetcher = new BitstampTransactionPullFetcher(bitstampMarketId)
 
     // Create Components
     val printer = builder.createRef(Props(classOf[Printer], "my-printer"))
-    val persistor = builder.createRef(Props(classOf[Persistor[Transaction]], btceXactPersit, implicitly[ClassTag[Transaction]]))
-    val fetcher = builder.createRef(Props(classOf[PullFetchComponent[Transaction]], btcePullFetcher, implicitly[ClassTag[Transaction]]))
+    val btcePersistor = builder.createRef(Props(classOf[Persistor[Transaction]], btceXactPersit, implicitly[ClassTag[Transaction]]))
+    val btceFetcher = builder.createRef(Props(classOf[PullFetchComponent[Transaction]], btcePullFetcher, implicitly[ClassTag[Transaction]]))
+    val bitstampPersistor = builder.createRef(Props(classOf[Persistor[Transaction]], bitstampXactPersit, implicitly[ClassTag[Transaction]]))
+    val bitstampFetcher = builder.createRef(Props(classOf[PullFetchComponent[Transaction]], bitstampPullFetcher, implicitly[ClassTag[Transaction]]))
 
     // Create the connections
-    fetcher.addDestination(printer, classOf[Transaction])
-    fetcher.addDestination(persistor, classOf[Transaction])
+    btceFetcher.addDestination(printer, classOf[Transaction])
+    btceFetcher.addDestination(btcePersistor, classOf[Transaction])
+    bitstampFetcher.addDestination(printer, classOf[Transaction])
+    bitstampFetcher.addDestination(bitstampPersistor, classOf[Transaction])
 
     // Start the system
     builder.start
