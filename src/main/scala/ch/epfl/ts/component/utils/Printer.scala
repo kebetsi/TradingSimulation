20853b352cb19f9343c8271420408c035a408f12
ikebets <ilia.kebets@epfl.ch>
2015-01-13 17:06:54 +0100

    add other messages + format

--- src/main/scala/ch/epfl/ts/component/utils/Printer.scala
+++ src/main/scala/ch/epfl/ts/component/utils/Printer.scala
@@ -1,7 +1,8 @@
 package ch.epfl.ts.component.utils
 
 import ch.epfl.ts.component.Component
-import ch.epfl.ts.data.{LimitOrder, DelOrder, LimitAskOrder, LimitBidOrder, Transaction, Tweet, LiveLimitAskOrder, LiveLimitBidOrder}
+import ch.epfl.ts.data.{ LimitOrder, DelOrder, LimitAskOrder, LimitBidOrder, Transaction, Tweet, LiveLimitAskOrder, LiveLimitBidOrder }
+import ch.epfl.ts.data.OHLC
 
 /**
  * Simple printer component for Transactions.
@@ -9,14 +10,16 @@ import ch.epfl.ts.data.{LimitOrder, DelOrder, LimitAskOrder, LimitBidOrder, Tran
  */
 class Printer(val name: String) extends Component {
   override def receiver = {
-    case t: Transaction    => println("Printer " + name + ": Transaction\t" + System.currentTimeMillis + "\t" + t.toString)
-    case t: Tweet          => println("Printer " + name + ": Tweet\t" + System.currentTimeMillis + "\t" + t.toString)
-    case lb: LimitBidOrder => println("Printer " + name + ": Limit Bid Order\t" + System.currentTimeMillis() + "\t" + lb.toString)
-    case la: LimitAskOrder => println("Printer " + name + ": Limit Ask Order\t" + System.currentTimeMillis() + "\t" + la.toString)
-    case del: DelOrder     => println("Printer " + name + ": Delete Order\t" + System.currentTimeMillis() + "\t" + del.toString)
+    case t: Transaction         => println("Printer " + name + ": Transaction\t" + System.currentTimeMillis + "\t" + t.toString)
+    case t: Tweet               => println("Printer " + name + ": Tweet\t" + System.currentTimeMillis + "\t" + t.toString)
+    case lb: LimitBidOrder      => println("Printer " + name + ": Limit Bid Order\t" + System.currentTimeMillis() + "\t" + lb.toString)
+    case la: LimitAskOrder      => println("Printer " + name + ": Limit Ask Order\t" + System.currentTimeMillis() + "\t" + la.toString)
+    case del: DelOrder          => println("Printer " + name + ": Delete Order\t" + System.currentTimeMillis() + "\t" + del.toString)
     case llb: LiveLimitBidOrder => println("Printer " + name + ": Live Limit Bid Order\t" + System.currentTimeMillis() + "\t" + llb.toString)
     case lla: LiveLimitAskOrder => println("Printer " + name + ": Live Limit Ask Order\t" + System.currentTimeMillis() + "\t" + lla.toString)
-    case os: List[LimitOrder] => println("Printer " + name + " LimitOrders"); os.map(x => println(x))
-    case _                 => println("Printer " + name + ": received unknown")
+    case ohlc: OHLC             => println("Printer " + name + ": OHLC\t" + System.currentTimeMillis() + "\t" + ohlc.toString)
+    case os: List[LimitOrder]   =>
+      println("Printer " + name + " LimitOrders"); os.map(x => println(x))
+    case _                      => println("Printer " + name + ": received unknown")
   }
 }
