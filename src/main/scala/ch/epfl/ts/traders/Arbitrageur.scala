3d839f913c04cd8036b209410cbbb68915d8847e
ikebets <ilia.kebets@epfl.ch>
2015-01-13 12:21:25 +0100

    arbitrageur compute price difference and message print.

--- src/main/scala/ch/epfl/ts/traders/Arbitrageur.scala
+++ src/main/scala/ch/epfl/ts/traders/Arbitrageur.scala
@@ -5,22 +5,36 @@ import ch.epfl.ts.data.Transaction
 import ch.epfl.ts.engine.OHLC
 import ch.epfl.ts.component.fetch.MarketNames
 
-class Arbitrageur(uid: Long, firstMid: Long, secondMid: Long) extends Component {
+class Arbitrageur(uid: Long) extends Component {
 
-  var firstTradingPrice: Double = 0.0
-  var secondTradingPrice: Double = 0.0
+  // key: marketId, value: tradingPrice
+  var marketPrices = Map[Long, Double]()
+  // key: marketId, value: most recent OHLC
+  var marketOhlcs = Map[Long, OHLC]()
 
   def receiver = {
     case t: Transaction => {
-      //println("Arbitrageur: received transaction: " + t)
-      t.mid match {
-        case `firstMid`  => firstTradingPrice = t.price
-        case `secondMid` => secondTradingPrice = t.price
-        case _ => println("Arbitrageur: unknown MarketId")
-      }
-      //println("Arbitrageur: firstP=" + firstTradingPrice + ", secondP=" + secondTradingPrice + ". diff=" + (firstTradingPrice-secondTradingPrice)/secondTradingPrice)
+      marketPrices += (t.mid -> t.price)
+      //      println("Arbitrageur: MarketPrices")
+      //      marketPrices.map(m => println("Market " + MarketNames.marketIdToName(m._1) + ": trading price = " + m._2))
     }
-    case ohlc: OHLC => println("Arbitrageur: received OHLC from " + MarketNames.marketIdToName(ohlc.marketId) + ": " + ohlc)
+    case ohlc: OHLC => {
+      marketOhlcs += (ohlc.marketId -> ohlc)
+      println("Arbitrageur: received OHLC from " + MarketNames.marketIdToName(ohlc.marketId) + ": " + ohlc)
+      computeOhlcDifferences
+    }
     case _ => println("Arbitrageur: received unknown")
+  }
+
+  def computeOhlcDifferences = {
+    marketOhlcs.map(a => marketOhlcs.map(b => {
+      if (a != b) {
+        print("market " + MarketNames.marketIdToName(a._1) + ": price = " + a._2.close + ", market " + MarketNames.marketIdToName(b._1) + 
+            ": price = " + b._2.close + ", difference = ")
+        print(f"${a._2.close - b._2.close}%.2f, ")
+        println(f"(${(a._2.close - b._2.close) / b._2.close}%.2f %%)")
+        // calculate win if sold on 1 market, and bought on other, take in account commission
+      }
+    }))
   }
 }
