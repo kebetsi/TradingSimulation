1c32a209b53fd03fc00ff0122bbbbed1f7c834b2
ikebets <ilia.kebets@epfl.ch>
2015-01-22 14:36:17 +0100

    clean and comment examples.

--- src/main/scala/ch/epfl/ts/example/MarketSimulatorTest.scala
+++ src/main/scala/ch/epfl/ts/example/MarketSimulatorTest.scala
@@ -1,4 +1,4 @@
-package ch.epfl.main
+package ch.epfl.ts.example
 
 import akka.actor.Props
 import ch.epfl.ts.component.utils.Printer
@@ -6,18 +6,47 @@ import ch.epfl.ts.component.{Component, ComponentBuilder, StartSignal}
 import ch.epfl.ts.data.Currency._
 import ch.epfl.ts.data.{LimitAskOrder, LimitBidOrder, MarketAskOrder, MarketBidOrder, Transaction}
 import ch.epfl.ts.engine.{MarketRules, MarketSimulator}
+import ch.epfl.ts.component.utils.Printer
+import ch.epfl.ts.data.MarketAskOrder
+import ch.epfl.ts.data.MarketBidOrder
+import ch.epfl.ts.data.Transaction
+import ch.epfl.ts.engine.MarketSimulator
 
+/**
+ * test setup of a MarketSimulator receiving orders and matching them when possible.
+ */
+object MarketSimulatorTest {
+
+  def main(args: Array[String]) {
+    // init builder
+    implicit val builder = new ComponentBuilder("MarketSimTestSystem")
+    
+    // create components
+    val market = builder.createRef(Props(classOf[MarketSimulator], 0L, new MarketRules()))
+    val tester = builder.createRef(Props(classOf[testOrdersSender]))
+    val printer = builder.createRef(Props(classOf[Printer], "MSprinter"))
+    
+    // create edges
+    market.addDestination(printer, classOf[Transaction])
+    tester.addDestination(market, classOf[LimitAskOrder])
+    tester.addDestination(market, classOf[LimitBidOrder])
+    tester.addDestination(market, classOf[MarketBidOrder])
+    tester.addDestination(market, classOf[MarketAskOrder])
+    
+    // start system
+    builder.start
+  }
+}
+
+/**
+ * simple component that sends a set of orders
+ */
 class testOrdersSender extends Component {
 
   override def receiver = {
     case StartSignal() => {
-      //      send(new LimitAskOrder(1, 1, 1, USD, 90, 50, USD))
 
-      send(new LimitBidOrder(2, 1, 8, USD, USD, 100, 90))
-
-      //      send(MarketAskOrder(3, 1, 2, USD, 0, 50, USD))
-      //            send(MarketBidOrder(3, 1, 2, USD, 0, 50, USD))
-      send(LimitAskOrder(1, 3, 3, USD, USD, 50, 110))
+      send(LimitBidOrder(2, 1, 8, USD, USD, 100, 90))
       send(LimitAskOrder(1, 3, 1, USD, USD, 50, 110))
       send(LimitAskOrder(1, 4, 4, USD, USD, 50, 120))
       send(LimitAskOrder(1, 5, 5, USD, USD, 50, 110))
@@ -27,47 +56,7 @@ class testOrdersSender extends Component {
 
     }
     case _ => {
-      print("Connector: unknown thing received: ")
+      print("testOrdersSender: unknown data received")
     }
-  }
-}
-
-object MarketSimulatorTest {
-
-  def main(args: Array[String]) {
-    println("daw")
-    //
-    //    val system = ActorSystem("marketSystem")
-    //    val market = system.actorOf(Props(new MarketSimulator), "market")
-
-    implicit val builder = new ComponentBuilder("ReplayFinanceSystem")
-    val market = builder.createRef(Props(classOf[MarketSimulator], new MarketRules()))
-    val tester = builder.createRef(Props(classOf[testOrdersSender]))
-    val printer = builder.createRef(Props(classOf[Printer], "ReplayLoopPrinter"))
-    market.addDestination(printer, classOf[Transaction])
-    tester.addDestination(market, classOf[LimitAskOrder])
-    tester.addDestination(market, classOf[LimitBidOrder])
-    tester.addDestination(market, classOf[MarketBidOrder])
-    tester.addDestination(market, classOf[MarketAskOrder])
-    builder.start
-
-    //    market ! new LimitAskOrder(1, 1, 1,USD, 100, 50, USD)
-    //    market ! new LimitAskOrder(1, 2, 2,USD, 90, 50, USD)
-    //    market ! new LimitAskOrder(1, 3, 3, USD, 110, 50, USD)
-    //    market ! new LimitAskOrder(1, 3, 1, USD, 110, 50, USD)
-    //    market ! new LimitAskOrder(1, 4, 4, USD, 120, 50, USD)
-    //    market ! new LimitAskOrder(1, 5, 5, USD, 110, 50, USD)
-    //    market ! new LimitAskOrder(1, 6, 6, USD, 140, 50, USD)
-    //    market ! new LimitAskOrder(1, 7, 7, USD, 80, 100, USD)
-
-    //    market ! PrintBooks
-
-    //    market ! new LimitBidOrder(2, 8, 8, USD, 90, 50, USD)
-    //    market ! new LimitBidOrder(2, 9, 11, USD, 110, 50, USD)
-    //    market ! new LimitBidOrder(2, 9, 5, USD, 110, 50, USD)
-    //    market ! new LimitBidOrder(2, 9, 9, USD, 110, 50, USD)
-    //    market ! new LimitBidOrder(2, 10, 10, USD, 100, 50, USD)
-
-    //    market ! PrintBooks
   }
 }
