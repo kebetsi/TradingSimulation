ea489c89e20409703825ec33e5f8a1821672d620
Elias Schegg <eliasschegg@gmail.com>
2015-03-25 19:03:13 +0100

    Implement basic WebSocket demo that sends TrueFX quotes as JSON to the console

--- frontend/public/javascripts/hello.js
+++ frontend/public/javascripts/hello.js
@@ -1,3 +1,14 @@
-if (window.console) {
-  console.log("Welcome to your Play application's JavaScript!");
+var ws = new WebSocket("ws://localhost:9000/wsTest");
+ws.onmessage = function(event) {
+	console.log("WS received TrueFx quotes:")
+	console.log(JSON.parse(event.data));
+}
+ws.onopen = function(event) { 
+	console.log("WS connection open"); 
+} 
+ws.onclose = function(event) { 
+	console.log("WS connection closed"); 
+} 
+ws.onerror = function(event) { 
+	console.log("WS Error"); 
 }
