24a020403a9c5f55122dc779debde0521103c228
Merlin Nimier-David <merlin.nimier@gmail.com>
2015-03-25 09:10:55 +0100

    Use `MovingAverageFXTrader` in the Forex market example

--- src/main/scala/ch/epfl/ts/example/SimpleExampleFX.scala
+++ src/main/scala/ch/epfl/ts/example/SimpleExampleFX.scala
@@ -1,27 +1,22 @@
 package ch.epfl.ts.example
 
 import ch.epfl.ts.component.ComponentBuilder
-import ch.epfl.ts.component.fetch.PullFetchComponent
+import ch.epfl.ts.engine.ForexMarketRules
 import ch.epfl.ts.component.fetch.TrueFxFetcher
-import ch.epfl.ts.data.LimitAskOrder
-import ch.epfl.ts.data.Quote
-import ch.epfl.ts.engine.MarketRules
-import ch.epfl.ts.engine.MarketSimulator
-import akka.actor.Props
-import scala.reflect.ClassTag
+import ch.epfl.ts.component.persist.DummyPersistor
 import ch.epfl.ts.component.fetch.MarketNames
-import ch.epfl.ts.traders.SimpleFXTrader
+import ch.epfl.ts.engine.MarketFXSimulator
+import akka.actor.Props
+import ch.epfl.ts.traders.MovingAverageFXTrader
+import ch.epfl.ts.component.utils.BackLoop
+import ch.epfl.ts.indicators.SmaIndicator
+import scala.reflect.ClassTag
+import ch.epfl.ts.component.fetch.PullFetchComponent
+import ch.epfl.ts.engine.RevenueComputeFX
+import ch.epfl.ts.data.Quote
+import ch.epfl.ts.data.Transaction
 import ch.epfl.ts.data.MarketAskOrder
 import ch.epfl.ts.data.MarketBidOrder
-import ch.epfl.ts.data.Quote
-import ch.epfl.ts.engine.RevenueCompute
-import ch.epfl.ts.data.LimitBidOrder
-import ch.epfl.ts.engine.MarketFXSimulator
-import ch.epfl.ts.data.Transaction
-import ch.epfl.ts.engine.RevenueComputeFX
-import ch.epfl.ts.engine.ForexMarketRules
-import ch.epfl.ts.component.utils.{BackLoop, Printer}
-import ch.epfl.ts.component.persist.DummyPersistor
 
 
 object SimpleExampleFX {
@@ -29,34 +24,47 @@ object SimpleExampleFX {
     implicit val builder = new ComponentBuilder("simpleFX")
     val marketForexId = MarketNames.FOREX_ID
 
-    //Creating actors:
-    //fetcher
+    // ----- Creating actors
+    // Fetcher
     val fetcherFx: TrueFxFetcher = new TrueFxFetcher
     val fxQuoteFetcher = builder.createRef(Props(classOf[PullFetchComponent[Quote]], fetcherFx, implicitly[ClassTag[Quote]]), "trueFxFetcher")
-    //market
+    
+    // Market
     val rules = new ForexMarketRules()
     val forexMarket = builder.createRef(Props(classOf[MarketFXSimulator], marketForexId, rules), MarketNames.FOREX_NAME)
-    //persistor
+    
+    // Persistor
     val dummyPersistor = new DummyPersistor()
-    //backloop
+    
+    // Backloop
     val backloop = builder.createRef(Props(classOf[BackLoop], marketForexId, dummyPersistor), "backloop")
-    //trader
-    val simpleFXTrader = builder.createRef(Props(classOf[SimpleFXTrader], 132L), "simpleFXTrader")
-    val traderNames = Map(132L->"simpleFXTrader")
-    //display
-    val display = builder.createRef(Props(classOf[RevenueComputeFX],traderNames), "display")
+    
+    // Trader: cross moving average
+    val traderId : Long = 123L
+    val volume = 50.0
+    val shortPeriod = 50
+    val longPeriod = 200
+    val trader = builder.createRef(Props(classOf[MovingAverageFXTrader], traderId, shortPeriod, longPeriod, volume), "simpleTrader")
+   
+    // Indicators
+    val smaShort = builder.createRef(Props(classOf[SmaIndicator], shortPeriod), "smaShort")
+    val smaLong = builder.createRef(Props(classOf[SmaIndicator], longPeriod), "smaLong")
+    
+    // Display
+    val traderNames = Map(traderId -> "MovingAverageFXTrader")
+    val display = builder.createRef(Props(classOf[RevenueComputeFX], traderNames), "display")
 
-    //connections:
+    // ----- Connecting actors
     fxQuoteFetcher.addDestination(forexMarket, classOf[Quote])
-    fxQuoteFetcher.addDestination(simpleFXTrader, classOf[Quote])
+    fxQuoteFetcher.addDestination(trader, classOf[Quote])
 
-    simpleFXTrader.addDestination(forexMarket, classOf[MarketAskOrder])
-    simpleFXTrader.addDestination(forexMarket, classOf[MarketBidOrder])
+    trader.addDestination(forexMarket, classOf[MarketAskOrder])
+    trader.addDestination(forexMarket, classOf[MarketBidOrder])
 
     forexMarket.addDestination(backloop, classOf[Transaction])
     forexMarket.addDestination(display, classOf[Transaction])
 
-    backloop.addDestination(simpleFXTrader, classOf[Transaction])
+    backloop.addDestination(trader, classOf[Transaction])
 
     builder.start
   }
