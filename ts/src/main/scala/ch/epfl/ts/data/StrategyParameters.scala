7bb902439359ceb1c09cb4f211accd61ed943ea1
Merlin Nimier-David <merlin.nimier@gmail.com>
2015-04-12 18:17:02 +0200

    Common interface for strategy parameters with auto value checking

--- ts/src/main/scala/ch/epfl/ts/data/StrategyParameters.scala
+++ ts/src/main/scala/ch/epfl/ts/data/StrategyParameters.scala
@@ -17,10 +17,50 @@ class StrategyParameters {
    // TODO
 }
 
-abstract class Parameter(val name: String) {
-  override def toString: String = name + "Parameter"
+/**
+ * Represents a generic trading strategy parameter.
+ * Strategies can declare required and optional parameters.
+ * Each parameter has a "range" of valid values.
+ * @param T Parameter value type
+ */
+abstract class Parameter[T](val name: String) {
+  /**
+   * At construction, ensure that the given value is legal for this parameter.
+   */
+  assert(isValid, "Illegal value " + get() + " for strategy parameter " + toString)
   
-  // TODO: other common fields and methods (valid range, parameters generation, etc)
+  /** Retrieve the value for this parameter */
+  def get(): T
+  
+  /** The companion object of this parameter */
+  protected val companion: ParameterTrait[T]
+  
+  /**
+   * Whether or not this particle instance has been
+   * parameterized with a legal value.
+   */
+  def isValid: Boolean = companion.isValid(get())
+  
+  override def toString: String = name + "Parameter"
+}
+
+/**
+ * "Static" methods that should be implemented in each concrete
+ * parameter's companion object.
+ */
+trait ParameterTrait[T] {
+  
+  /**
+   * Should be implemented in each concrete strategy parameter
+   * @return Whether the value is suitable for this parameter
+   */
+  def isValid(value: T): Boolean
+
+  /**
+   * Generate the range of valid values for this parameter.
+   * TODO: How to handle infinite ranges? Use streams?
+   */
+  def validValues: Iterable[T]
 }
 
 case class CurrencyParameter(currency: Currency) extends Parameter("Currency")
