26f9610d5dcdde2f46594e9c0145bd494e68791e
Merlin Nimier-David <merlin.nimier@gmail.com>
2015-04-20 11:27:41 +0200

    `MadTrader` test class

---
+++ ts/src/test/scala/ch/epfl/ts/test/traders/MadTraderTest.scala
@@ -0,0 +1,43 @@
+package ch.epfl.ts.test.traders
+
+import scala.language.postfixOps
+import scala.util.Try
+import org.junit.runner.RunWith
+import org.scalatest.FunSuiteLike
+import org.scalatest.WordSpecLike
+import org.scalatest.junit.JUnitRunner
+import akka.actor.ActorSystem
+import akka.actor.Props
+import akka.testkit.TestKit
+import ch.epfl.ts.data.Currency
+import ch.epfl.ts.data.CurrencyPairParameter
+import ch.epfl.ts.data.NaturalNumberParameter
+import ch.epfl.ts.data.StrategyParameters
+import ch.epfl.ts.test.TestHelpers
+import ch.epfl.ts.traders.MadTrader
+import ch.epfl.ts.data.TimeParameter
+import scala.concurrent.duration.DurationLong
+
+@RunWith(classOf[JUnitRunner])
+class MadTraderTest
+  extends TestKit(TestHelpers.makeTestActorSystem("MadTraderTest"))
+  with FunSuiteLike {
+
+  val traderId = 42L
+  val emptyParameters = new StrategyParameters()
+  val legalParameters = new StrategyParameters(
+      MadTrader.CURRENCY_PAIR -> CurrencyPairParameter(Currency.EUR, Currency.CHF),
+      MadTrader.INTERVAL -> new TimeParameter(1 seconds),
+      MadTrader.ORDER_VOLUME -> NaturalNumberParameter(100)
+  )
+    
+  test("Should not allow instantiation without any parameters") {
+    val attempt = Try( MadTrader.getInstance(traderId, emptyParameters) )
+    assert(attempt.isFailure)
+  }
+  
+  test("Should allow instantiation with only required parameters") {
+	  val attempt = Try( MadTrader.getInstance(traderId, legalParameters) )
+		assert(attempt.isSuccess)
+  }
+}
