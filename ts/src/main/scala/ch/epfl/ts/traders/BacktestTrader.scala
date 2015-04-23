25fc77f1ff4e35b3f14907bd486b88022d99aa88
liu fengyun <liufengyunchina@gmail.com>
2015-04-23 19:06:24 +0200

    update statistics use new formula and calculate in periods

--- ts/src/main/scala/ch/epfl/ts/traders/BacktestTrader.scala
+++ ts/src/main/scala/ch/epfl/ts/traders/BacktestTrader.scala
@@ -1,29 +1,94 @@
 package ch.epfl.ts.traders
 
+import scala.collection.mutable.{Map => MMap, MutableList => MList}
+import scala.concurrent.duration.DurationInt
+import akka.actor.Cancellable
 import ch.epfl.ts.component.{Component, ComponentRef}
 import ch.epfl.ts.data._
 import ch.epfl.ts.data.Currency._
 
 /**
- * Implements back testing for traders.
- *
- * To use this class, redirect all previous connections into trader to instances
- * of this class. Connections out of the original trader should remain unchanged.
- *
- * @param traderId the id of the trader
- * @param trader the reference to the real trader component
+  * Implements back testing for traders.
+  *
+  * To use this class, redirect all previous connections into trader to instances
+  * of this class. Connections out of the original trader should remain unchanged.
+  *
+  * @param trader the reference to the real trader component
+  * @param traderId the id of the trader
+  * @param initial the initial seed money
+  * @param currency currency of the intial seed money
+  * @param period the period in milliseconds to compute performance
  */
-class BacktestTrader(traderId: Long, wallet: Map[Currency, Double], trader: ComponentRef) extends Component {
-  def returns: Double = ???
-  def volatility: Double = ???
-  def drawdown: Double = ???
-  def sharp: Double = ???
+class BacktestTrader(trader: ComponentRef, traderId: Long, initial: Double, currency: Currency, period: Long) extends Component {
+  private var schedule: Cancellable = null
+  private val wallet = MMap[Currency, Double](initial)
+  private val returns = MList[Double]()
 
+  private val lastValue = initial
+  private val maxProfit = 0.0
+  private val maxLoss = 0.0
+
+  def totalReturns: Double = (value - initial) * 100
+  def volatility: Double = computeVolatility
+  def drawdown: Double = maxLoss
+  def sharp: Double = totalReturns / volatility
+
+  // handle interested messages and forward all messages to the trader
   override def receiver = {
     case t: Transaction if t.buyerId == traderId =>  // buy
       trader.ar ! t
-    case t: Transaction if t.sellerId == traderId => // sell
+      buy(t)
+    case t: Transaction if t.sellerId == traderId =>  // sell
       trader.ar ! t
+      sell(t)
+    case 'UpdateStatistics =>
+      updateStatistic
     case m => trader.ar ! m
   }
+
+  // returns the exchange ratio between two currency
+  private def ratio(from: Currency, to: Currency): Double = ???
+
+  // returns the total money of the wallet converted to the given currency
+  private def value(in: Currency = currency): Double = {
+    (wallet :\ 0.0) { case ((c, amount), acc) => acc + ratio(c, in) * amount }
+  }
+
+  // updates the wallet and statistics after a sell contraction
+  private def sell(t: Transaction): Unit = {
+    wallet[t.whatC] -= t.volume
+    wallet[t.withC] += t.volume * t.price
+  }
+
+  // updates the wallet and statistics after a buy contraction
+  private def buy(t: Transaction): Unit = {
+    wallet[t.whatC] += t.volume
+    wallet[t.withC] -= t.volume * t.price
+  }
+
+  // compute volatility, which is the variance of returns
+  private def computeVolatility = {
+    val mean = (returns :\ 0)(_ + _) / returns.length
+      (returns :\ 0) { (r, acc) => (r - mean) * (r - mean) + acc } / returns.length
+  }
+
+  // updates the statistics
+  private def updateStatistic = {
+    val curVal = value
+
+    val profit = curVal - initial
+    if (profit > maxProfit) maxProfit = profit
+    else if (profit < maxLoss) maxLoss = profit
+    else ()
+
+    returns += (curVal - lastValue) / lastValue
+
+    lastValue = curVal
+  }
+
+  override def start = {
+    schedule = system.scheduler.schedule(10 milliseconds, period milliseconds, self, 'UpdateStatistics)
+  }
+
+  override def stop = schedule.cancel()
 }
