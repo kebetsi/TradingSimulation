1bb10f3b859c6fd35be1a573bdf46b9ce56ce994
Jakob Ehrl <jakobehrl@gmail.com>
2015-04-28 18:25:04 +0200

    [VISUALIZATION] UI added tabs

--- frontend/app/assets/javascripts/app.js
+++ frontend/app/assets/javascripts/app.js
@@ -1,143 +1,159 @@
 var myApp = angular.module('myApp', [ 'ngRoute' ]);
 
-myApp.controller('MyController', [
-    '$scope',
-    function($scope) {
-      $scope.greeting = 'it works';
-      $scope.currencies = [];
-      
-      // put code for highchart here
-      var myChart; // global variable to access chart
+myApp.controller('MyController', [ '$scope', function($scope) {
+  $scope.greeting = 'it works';
+  $scope.currencies = [];
 
-      $(function () {
+  // put code for highchart here
+  var myChart; // global variable to access chart
 
-        Highcharts.setOptions({
-          global : {
-            useUTC : false
-          }
-        });
+  $(function() {
 
-        // Create the chart
-//        $('#container').highcharts('StockChart', {
-        myChart = new Highcharts.StockChart({
-          chart : {
-            renderTo : 'container'//,
-//            events : {
-//              load : function () {
-      //
-//                // set up the updating of the chart each second
-//                var series = this.series[0];
-//                setInterval(function () {
-//                  var x = (new Date()).getTime(), // current time
-//                    y = Math.round(Math.random() * 100);
-//                  series.addPoint([x, y], true, true);
-//                }, 1000);
-//              }
-//            }
-          },
-
-          rangeSelector: {
-            buttons: [{
-              count: 1,
-              type: 'minute',
-              text: '1M'
-            }, {
-              count: 5,
-              type: 'minute',
-              text: '5M'
-            }, {
-              type: 'all',
-              text: 'All'
-            }],
-            inputEnabled: false,
-            selected: 0
-          },
-
-          title : {
-            text : 'Live stock data'
-          },
-
-          exporting: {
-            enabled: false
-          },
-
-          series : (function () {
-            // generate 10 series with name "i" and empty data
-            var seriesTmp = [];
-
-            for (var i = 0; i < 10; ++i) {
-              seriesTmp.push({name : i.toString(), data : [0, 0]});
-            }
-            return seriesTmp;
-          }()) 
-        });
-        
-//        $('#button').click(function () {
-//          var chart = $('#container').highcharts(),
-//            series = chart.series[0];
-//          if (series.visible) {
-//            series.hide();
-//          } else {
-//            series.show();
-//          }
-//        });
-
-      });
-
-      function toggleGraph(id) {
-//        console.log(this);
-        var button_id = id.split("_")[1];
-        series = myChart.series[button_id];
-        if (series.visible) {
-          series.hide();
-        } else {
-          series.show();
-        }
+    Highcharts.setOptions({
+      global : {
+        useUTC : false
       }
+    });
 
-      var ws = new WebSocket("ws://localhost:9000/wsTest");
-      ws.onmessage = function(event) {
-//        console.log("WS received TrueFx quotes:");
-        
-        var quotes = JSON.parse(event.data).quotes;
-//        console.log(dataParsed);
-        
-        var fill = false;
-//        // initialize symbol names
-        if ($scope.currencies.length < 1) {
-          fill = true;
+    // Create the chart
+    // $('#container').highcharts('StockChart', {
+    myChart = new Highcharts.StockChart({
+      chart : {
+        renderTo : 'container'// ,
+      // events : {
+      // load : function () {
+      //
+      // // set up the updating of the chart each second
+      // var series = this.series[0];
+      // setInterval(function () {
+      // var x = (new Date()).getTime(), // current time
+      // y = Math.round(Math.random() * 100);
+      // series.addPoint([x, y], true, true);
+      // }, 1000);
+      // }
+      // }
+      },
+
+      rangeSelector : {
+        buttons : [ {
+          count : 1,
+          type : 'minute',
+          text : '1M'
+        }, {
+          count : 5,
+          type : 'minute',
+          text : '5M'
+        }, {
+          type : 'all',
+          text : 'All'
+        } ],
+        inputEnabled : false,
+        selected : 0
+      },
+
+      title : {
+        text : 'Live stock data'
+      },
+
+      exporting : {
+        enabled : false
+      },
+
+      series : (function() {
+        // generate 10 series with name "i" and empty data
+        var seriesTmp = [];
+
+        for (var i = 0; i < 10; ++i) {
+          seriesTmp.push({
+            name : i.toString(),
+            data : [ 0, 0 ]
+          });
         }
+        return seriesTmp;
+      }())
+    });
 
-        var x = (new Date()).getTime(); // current time for debugging (fetcher always returns same time step)
-        console.log(quotes.length);
-        for (var i = 0; i < quotes.length; /*i += 2*/ ++i) {
-          var whatC = quotes[i].whatC;
-          var withC = quotes[i].withC;
-          var y = quotes[i].ask;
-//          x = quotes[i].timestamp; // makes the whole graph 
-          var series = myChart.series[i];
-          series.name = whatC + " to " + withC;
-          if (fill)
-            {
-            $scope.currencies.push(whatC + " to " + withC);
-            console.log($scope.currencies);
-            }
-          var shift = series.data.length > 20 ? true : false;
-//          console.log(series.data.length);
-          series.addPoint([x, y], false, shift); // bool1: redraw, bool2: shift  
-        }
+    // $('#button').click(function () {
+    // var chart = $('#container').highcharts(),
+    // series = chart.series[0];
+    // if (series.visible) {
+    // series.hide();
+    // } else {
+    // series.show();
+    // }
+    // });
 
-        myChart.redraw();
-        
+  });
+
+  function toggleGraph(id) {
+    // console.log(this);
+    var button_id = id.split("_")[1];
+    series = myChart.series[button_id];
+    if (series.visible) {
+      series.hide();
+    } else {
+      series.show();
+    }
+  }
+
+  var ws = new WebSocket("ws://localhost:9000/wsTest");
+  ws.onmessage = function(event) {
+    // console.log("WS received TrueFx quotes:");
+
+    var quotes = JSON.parse(event.data).quotes;
+    // console.log(dataParsed);
+
+    var fill = false;
+    // initialize symbol names
+    if ($scope.currencies.length < 10) {
+      fill = true;
+    }
+
+    var x = (new Date()).getTime(); // current time for debugging (fetcher
+                                    // always returns same time step)
+    console.log(quotes.length);
+    for (var i = 0; i < quotes.length; /* i += 2 */++i) {
+      var whatC = quotes[i].whatC;
+      var withC = quotes[i].withC;
+      var y = quotes[i].ask;
+      // x = quotes[i].timestamp; // makes the whole graph
+      var series = myChart.series[i];
+      series.name = whatC + " to " + withC;
+      if (fill) {
+        $scope.currencies.push(whatC + " to " + withC);
 //        console.log($scope.currencies);
-      };
-      ws.onopen = function(event) { 
-        console.log("WS connection open"); 
-      };
-      ws.onclose = function(event) { 
-        console.log("WS connection closed"); 
-      };
-      ws.onerror = function(event) { 
-        console.log("WS Error"); 
-      };
-    } ]);
+      }
+      var shift = series.data.length > 20 ? true : false;
+      // console.log(series.data.length);
+      series.addPoint([ x, y ], false, shift); // bool1: redraw, bool2: shift
+    }
+
+    myChart.redraw();
+    $scope.$apply(); // this makes sure that the bound variables are updated
+                      // even though we are in an asynchronous call
+    // http://www.jeffryhouser.com/index.cfm/2014/6/2/How-do-I-run-code-when-a-variable-changes-with-AngularJS
+
+    // console.log($scope.currencies);
+  };
+  ws.onopen = function(event) {
+    console.log("WS connection open");
+  };
+  ws.onclose = function(event) {
+    console.log("WS connection closed");
+  };
+  ws.onerror = function(event) {
+    console.log("WS Error");
+  };
+} ]);
+
+// table controller
+myApp.controller('TabController', [ '$scope', function($scope) {
+  this.tab = 1;
+
+  this.selectTab = function(setTab) {
+    this.tab = setTab;
+  };
+  this.isSelected = function(checkTab) {
+    return this.tab === checkTab;
+  };
+} ]);
