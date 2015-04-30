a8b0e3707663daea8c209e4708e25a9360ba7bcb
Merlin Nimier-David <merlin.nimier@gmail.com>
2015-04-30 12:05:24 +0200

    Style

--- ts/src/main/scala/ch/epfl/ts/component/persist/QuotePersistor.scala
+++ ts/src/main/scala/ch/epfl/ts/component/persist/QuotePersistor.scala
@@ -26,30 +26,29 @@ class QuotePersistor(dbFilename: String) extends Persistance[Quote]{
     def withC: Column[String] = column[String]("WITH_C")
     def bid: Column[Double] = column[Double]("BID")
     def ask: Column[Double] = column[Double]("ASK")
-    }
+  }
   
   // Open DB session and query handler on the QUOTES table
   val db = Database.forURL("jdbc:sqlite:data/" + dbFilename + ".db", driver = "org.sqlite.JDBC")
   lazy val QuotesTable = TableQuery[Quotes]
-  db.withDynSession { if (MTable.getTables("QUOTES").list.isEmpty) { QuotesTable.ddl.create } }
+  db.withDynSession {
+    if (MTable.getTables("QUOTES").list.isEmpty) {
+      QuotesTable.ddl.create
+    }
+  }
   
   
   override def loadBatch(startTime: Long, endTime: Long): List[Quote] = db.withDynSession {
     val res = QuotesTable.filter(e => e.timestamp >= startTime && e.timestamp <= endTime).invoker
     res.list.map( r => Quote(MarketNames.FOREX_ID, r._2, Currency.fromString(r._3), Currency.fromString(r._4), r._5, r._6))
   }
+  // TODO
+  def loadSingle(id: Int): ch.epfl.ts.data.Quote = ???
 
   override def save(newQuotes: List[Quote]): Unit = db.withDynSession {
     // The first field of the quote (QUOTE_ID) is set to -1 but this will be 
     // ignored and auto incremented by jdbc:sqlite in the actual DB table.
     QuotesTable ++= newQuotes.map(q => (-1, q.timestamp, q.whatC.toString, q.withC.toString, q.bid, q.ask))
   }
-  
-  //TODO
-  def loadSingle(id: Int): ch.epfl.ts.data.Quote = ???
-  
-  def save(q: ch.epfl.ts.data.Quote): Unit = db.withDynSession {
-    save(List(q))
-  }
-
+  def save(q: ch.epfl.ts.data.Quote): Unit = save(List(q))
 }
