6a774903c24f0a7e760774ff62da7229ccc09538
Elias Schegg <eliasschegg@gmail.com>
2015-05-05 16:39:55 +0200

    Display message to the user if the WebService connection is lost

--- frontend/app/assets/javascripts/app.js
+++ frontend/app/assets/javascripts/app.js
@@ -1,118 +1,37 @@
-var myApp = angular.module('myApp', [ 'ngRoute', 'ui.bootstrap' ]);
+(function() {
+  'use strict';
 
-myApp.controller('MyController', [ '$scope', function($scope) {
-  $scope.currencies = [];
+  var app = angular.module('myApp', [ 'ngRoute', 'ui.bootstrap', 'angular-websocket' ]);
 
-  // put code for highchart here
-  var myChart; // global variable to access chart
 
-  $(function() {
-
-    Highcharts.setOptions({
-      global : {
-        useUTC : false
-      }
-    });
-
-    // Create the chart
-    myChart = new Highcharts.StockChart({
-      chart : {
-        renderTo : 'container'
-      },
-
-      rangeSelector : {
-        buttons : [ {
-          count : 1,
-          type : 'minute',
-          text : '1M'
-        }, {
-          count : 5,
-          type : 'minute',
-          text : '5M'
-        }, {
-          type : 'all',
-          text : 'All'
-        } ],
-        inputEnabled : false,
-        selected : 0
-      },
-
-      title : {
-        text : 'Live stock data'
-      },
-
-      exporting : {
-        enabled : false
-      },
-
-      series : (function() {
-        // generate 10 series with name "i" and empty data
-        var seriesTmp = [];
-
-        for (var i = 0; i < 10; ++i) {
-          seriesTmp.push({
-            name : i.toString(),
-            data : [ 0, 0 ]
+  
+  app.controller('MyController', [ '$scope', 'alertService',
+      function($scope, alertService) {
+        $scope.alerts = alertService.get();
+        
+        $scope.quotes = [];
+        
+        var ws = new WebSocket('ws://localhost:9000/fetchers/quote');
+        
+        ws.onmessage = function(event) {
+          var quote = JSON.parse(event.data);
+          $scope.$apply(function() {
+            console.log(quote);
           });
-        }
-        return seriesTmp;
-      }())
-    });
+        };
+        
+        ws.onclose = function(event) {
+          $scope.$apply(function() {
+            alertService.add('info', 'Closed connection to the backend');
+          });
+        };
+        
+        ws.onerror = function(event) {
+          $scope.$apply(function() {
+            alertService.add('danger', 'Lost connection to the backend');
+          });
+        };
+        
+   } ]);
 
-
-  });
-
-  function toggleGraph(id) {
-    var button_id = id.split("_")[1];
-    series = myChart.series[button_id];
-    if (series.visible) {
-      series.hide();
-    } else {
-      series.show();
-    }
-  }
-
-  var ws = new WebSocket("ws://localhost:9000/wsTest");
-  ws.onmessage = function(event) {
-    var quotes = JSON.parse(event.data).quotes;
-    var fill = false;
-    // initialize symbol names
-    if ($scope.currencies.length < 10) {
-      fill = true;
-    }
-    
-    quotes.foreach(function(quote) {
-      console.log(quote.timestamp);
-    });
-
-    var x = quotes[0].timestamp;  // note: timestamps for different symbols are not necessarily equal
-                                  // taking only one timestamp makes the graph faster and smoother for the cost of accuracy
-    for (var i = 0; i < quotes.length; /* i += 2 */++i) {
-      var whatC = quotes[i].whatC;
-      var withC = quotes[i].withC;
-      var y = quotes[i].ask;
-      var series = myChart.series[i];
-      if (fill) {
-        series.name = whatC + " to " + withC;
-        $scope.currencies.push(whatC + " to " + withC);
-      }
-      var shift = (series.data.length > 20);
-      series.addPoint([ x, y ], false, shift); // bool1: redraw, bool2: shift
-    }
-
-    myChart.redraw();
-    $scope.$apply(); // this makes sure that the bound variables are updated
-                      // even though we are in an asynchronous call
-    // http://www.jeffryhouser.com/index.cfm/2014/6/2/How-do-I-run-code-when-a-variable-changes-with-AngularJS
-  };
-  ws.onopen = function(event) {
-    console.log("WS connection open");
-  };
-  ws.onclose = function(event) {
-    console.log("WS connection closed");
-  };
-  ws.onerror = function(event) {
-    console.log("WS Error");
-  };
-} ]);
-
+})();
