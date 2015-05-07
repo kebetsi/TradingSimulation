47bed98ad7900d66d10d97c7f3e10fcc0e1c8e11
Dennis Meier <dennis.meier@epfl.ch>
2015-05-07 10:02:36 +0200

    Added an update-restart command to workerctl.sh

--- scripts/workerctl.sh
+++ scripts/workerctl.sh
@@ -23,9 +23,29 @@ BRANCH="run-on-cluster"
 # Which class to run on workers
 CLASS="ch.epfl.ts.remoting.RemotingActorExample"
 
+# Which logfile to write to on workers
 LOGFILE_ON_WORKER="~/RemotingActor.log"
 
 bold=$(tput bold) ; normal=$(tput sgr0) # Text formatting
+
+# Commands
+# The start command does:
+#	1. "ssh -f -n <server> <command>" connects to that server and executes the specified command
+#	2. "sh -c <command2>"" executes command2 in a shell environment
+#	3. "nohup <command3>" makes sure that command3 is not killed when its parent is killed 
+#		(in our case: sh will be killed as soon as ssh has sent its command and disconnects, 
+#		but nohup assures this doesn't happen to sbt)
+#	4. "sbt 'project ts' 'runMain ch.epfl.ts.remoting.RemotingActorExample'" runs our remote actor system
+#	5. " > ~/RemoteActor.log 2>&1" redirects all sbt output to ~/RemoteActor.log
+START_CMD="sh -c \"\cd ~/TradingSimulation; \
+			nohup sbt 'project ts' 'runMain $CLASS' \
+			> $LOGFILE_ON_WORKER 2>&1 &\""
+
+STOP_CMD="kill \$(ps -ef | grep '$CLASS' | grep -v grep | awk '{print \$2}')"
+
+STATUS_CMD="ps -ef | grep '$CLASS' | grep -v grep"
+
+UPDATE_CMD="sh -c 'cd ~/TradingSimulation; git fetch; git checkout origin/$BRANCH'"
 
 for i in "${!HOSTS[@]}"; do
 	HOST=${HOSTS[$i]}
@@ -35,31 +55,26 @@ for i in "${!HOSTS[@]}"; do
 
 	case "$1" in
 		start)
-			# The following long line does:
-			#	1. "ssh -f -n <server> <command>" connects to that server and executes the specified command
-			#	2. "sh -c <command2>"" executes command2 in a shell environment
-			#	3. "nohup <command3>" makes sure that command3 is not killed when its parent is killed 
-			#		(in our case: sh will be killed as soon as ssh has sent its command and disconnects, 
-			#		but nohup assures this doesn't happen to sbt)
-			#	4. "sbt 'project ts' 'runMain ch.epfl.ts.remoting.RemotingActorExample'" runs our remote actor system
-			#	5. " > ~/RemoteActor.log 2>&1" redirects all sbt output to ~/RemoteActor.log
-			ssh $USER@$HOST$DOMAINNAME -p $PORT "sh -c \"\cd ~/TradingSimulation; \
-			nohup sbt 'project ts' 'runMain $CLASS' \
-			> $LOGFILE_ON_WORKER 2>&1 &\""
-
+			ssh $USER@$HOST$DOMAINNAME -p $PORT "$START_CMD"
 			echo "Remote actor started. Logs are at $HOST$DOMAINNAME:$LOGFILE_ON_WORKER"
 			;;
 		stop)
-			ssh $USER@$HOST$DOMAINNAME -p $PORT "kill \$(ps -ef | grep '$CLASS' | grep -v grep | awk '{print \$2}')"
+			ssh $USER@$HOST$DOMAINNAME -p $PORT "$STOP_CMD"
 			echo "Killed remote actors."
 			;;
 		status)
 			echo "${bold}ps -ef | grep '$CLASS'${normal} on $HOST$DOMAINNAME:"
-			ssh $USER@$HOST$DOMAINNAME -p $PORT "ps -ef | grep '$CLASS' | grep -v grep"
+			ssh $USER@$HOST$DOMAINNAME -p $PORT "$STATUS_CMD"
 			;;
 		update)
 			echo "Updating from git branch $BRANCH"
-			ssh $USER@$HOST$DOMAINNAME -p $PORT "sh -c 'cd ~/TradingSimulation; git fetch; git checkout origin/$BRANCH'"
+			ssh $USER@$HOST$DOMAINNAME -p $PORT "$UPDATE_CMD"
+			;;
+		update-restart)
+			echo "Updating and restarting..."
+			ssh $USER@$HOST$DOMAINNAME -p $PORT "$UPDATE_CMD"
+			ssh $USER@$HOST$DOMAINNAME -p $PORT "$STOP_CMD"
+			ssh $USER@$HOST$DOMAINNAME -p $PORT "$START_CMD"
 			;;
 		*)
 			echo "Unknown action $1"
